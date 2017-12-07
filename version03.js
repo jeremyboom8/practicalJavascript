f8c9a24624ba7072bc347990e2f3b53c2f28cffc
Jeremy Boom <jeremy.boom77@gmail.com>
2017-12-07 21:34:17 +0100

    Added addTodo

--- version03.js
+++ version03.js
@@ -11,3 +11,14 @@ var jeremy = {
     console.log(this.name);
   }
 }
+
+var todoList = {
+  todos: ['item1', 'item2', 'item3'],
+  displayTodos: function() {
+    console.log('My todos:', this.todos);
+  },
+  addTodo: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  }
+}
