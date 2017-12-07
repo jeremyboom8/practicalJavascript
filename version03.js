0694c98a5204d5042400b85ebd4d4c4e75a06ce5
Jeremy Boom <jeremy.boom77@gmail.com>
2017-12-07 21:43:10 +0100

    Added DeleteTodo

--- version03.js
+++ version03.js
@@ -20,5 +20,13 @@ var todoList = {
   addTodo: function(todo) {
     this.todos.push(todo);
     this.displayTodos();
+  },
+  changeTodo: function(position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
   }
-}
+};
