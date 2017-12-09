b7604617145ccde87f7bc01c2401cf7cf12ba9e4
Jeremy Boom <jeremy.boom77@gmail.com>
2017-12-09 19:32:41 +0100

    Modified changeTodo to support objects

---
+++ version04.js
@@ -0,0 +1,21 @@
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+    console.log('My todos:', this.todos);
+  },
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
+};
